62d807aa1923311eaa7c098a5f4afd9537996c4a
Néville Dubuis <ndubuis@gmail.com>
2018-05-22 12:02:50 +0200

    comment api

--- app/website.rb
+++ app/website.rb
@@ -28,6 +28,52 @@ module Application
 
     # @api public
     # List  available trips
+    #
+    # example reponse:
+    #   {
+    #     "trips": [
+    #       {
+    #         "name": "EUX1811",
+    #         "description": "Abu Dhab - Dubai",
+    #         "start_date": "2018-05-13",
+    #         "end_date": "2018-05-22",
+    #         "language": "de",
+    #         "updated_at": "2018-04-03T07:59:08.780Z",
+    #         "type": "trip",
+    #         "image": "",
+    #         "bookings": [
+    #           {
+    #             "username": "GUEST",
+    #             "authentication_token": "HLKFEUX1811-DE",
+    #             "updated_at": "2017-05-12T09:08:54.958Z"
+    #            }
+    #          ],
+    #          "guides": [
+    #            {
+    #              "id": "2044.02",
+    #              "url": "...",
+    #              "generated_at": "2017-11-21T09:00:22.000+00:00",
+    #              "name": "Katar",
+    #              "description": "Miniguide",
+    #              "size": 1102519,
+    #              "language": "de"
+    #            },
+    #            {
+    #              "id": "2045.02",
+    #              "url": "...",
+    #              "generated_at": "2017-11-21T09:00:35.000+00:00",
+    #              "name": "Bahrain",
+    #              "description": "Miniguide",
+    #              "size": 2164075,
+    #              "language": "de"
+    #            }
+    #          ]
+    #        },
+    #        ...
+    #      ]
+    #    }
+    #
+    # @note be carefule, guide url in this response are the url to the destinationbook storage, the links are valid for 30 minutes. they must not be used.
     get '/trips' do
       File.read('public/trips.json')
     end
@@ -35,7 +81,15 @@ module Application
     # @api public
     # List available guides
     #
-    # @note retrun json
+    # example response:
+    #   [
+    #     {
+    #       'id': '1507.00',
+    #       'path': 'guides/1507.00'
+    #       'updated_at': '2018-05-17 16:57:47 +0200'
+    #     },
+    #     ...
+    #   ]
     get '/guides' do
       Guide.all.to_json
     end
@@ -43,7 +97,78 @@ module Application
     # @api public
     # The content of a guide
     #
-    # @param [String] guide_id the id of a guide
+    # example response:
+    #    {
+    #      "id": "1507.00",
+    #      "titleImage": "images/03821-USA_East_capitole__thumb.jpg",
+    #      "description": "Miniguide",
+    #      "title": "Washington, D.C.",
+    #      "headerImage": "images/03821-USA_East_capitole.jpg",
+    #      "children": [
+    #        {
+    #          "children": [
+    #            {
+    #              "children": [],
+    #              "linkTarget": null,
+    #              "link": {
+    #                "target": null,
+    #                "options": {
+    #                  "markGeo": null
+    #                }
+    #              },
+    #              "linkAnchors": [
+    #                "144553"
+    #              ],
+    #             "title": "17th century",
+    #             "content": "<p>html tagged content <img src=\"images/10543-USA_East_pocahontas.jpg\" title=\"Jamestown remembers the Indian chief’s daughter Pocahontas.\"><span class=\"legend\">Jamestown remembers the Indian chief’s daughter Pocahontas.</span></div>"
+    #           },
+    #           {
+    #             "children": [],
+    #             "linkTarget": null,
+    #             "link": {
+    #               "target": null,
+    #               "options": {
+    #                 "markGeo": null
+    #               }
+    #             },
+    #             "linkAnchors": [
+    #               "144554"
+    #             ],
+    #             "title": "18th century",
+    #             "content": "<p>html tagged content</p>"
+    #           }
+    #         ],
+    #       "linkTarget": null,
+    #       "link": {
+    #       "target": null,
+    #           "options": {
+    #             "markGeo": null
+    #           }
+    #         },
+    #         "linkAnchors": [
+    #           "144552"
+    #         ],
+    #         "title": "A Brief History",
+    #         "headerImageLegend": "The Legend of the image.",
+    #         "headerImage": "images/10511-USA_East_timbre.jpg"
+    #       },
+    #     ],
+    #     "images": [
+    #       {
+    #         "path": "images/03821-USA_East_capitole.jpg""
+    #       },
+    #       {
+    #         "path": "images/10543-USA_East_pocahontas.jpg"
+    #       },
+    #       {
+    #         "path": "images/10511-USA_East_timbre.jpg"
+    #       }
+    #     ]
+    #   }
+    #
+    # - "children" keys represent chapter, the include other chapters recursively
+    # - "images" key include all the image used in guide, images may exists in a thumb version (with __thumb after filename)
+    # - all link to images are relative to guide path
     get '/guides/:guide_id' do
       puts params['guide_id']
       Guide.new(params['guide_id']).content
@@ -53,6 +178,9 @@ module Application
 
     # @api public
     # Files relative to a guides
+    #
+    # for example:
+    #    /guides/1507.01/images/10511-USA_East_timbre.jpg
     get '/guides/:guide_id/:type/:file' do
       guide = Guide.new(params['guide_id'])
 
